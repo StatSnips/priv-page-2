eb308c98bdd77abbd0396ecba064a3b5c085252a
Alvin Thomas <alvin@statsnips.com>
2022-03-31 22:40:19 -0400

    Update specs for CI

--- spec/session_spec.cr
+++ spec/session_spec.cr
@@ -1,27 +1,27 @@
 require "spec"
 require "../src/session"
 
-describe PrivPage::Session do
+describe PrivPage2::Session do
   it "adds a session" do
-    session = PrivPage::Session(String).new
+    session = PrivPage2::Session(String).new
     session.add("1", "session").should eq "session"
     session.size.should eq 1
   end
 
   it "gets a session" do
-    session = PrivPage::Session(String).new
+    session = PrivPage2::Session(String).new
     session.add "1", "session"
     session.get?("1").should eq "session"
   end
 
   it "gets no session" do
-    session = PrivPage::Session(String).new
+    session = PrivPage2::Session(String).new
     session.get?("1").should be_nil
   end
 
   describe "garbage collecter" do
     it "removes old sessions" do
-      session = PrivPage::Session(String).new File.open File::NULL, "w"
+      session = PrivPage2::Session(String).new File.open File::NULL, "w"
       time = Time.utc
       session.add "1", "session", time: time - 2.days
       session.start_gc interval: 0.days, max_period: 1.days
@@ -30,7 +30,7 @@ describe PrivPage::Session do
     end
 
     it "keeps sessions not old enough" do
-      session = PrivPage::Session(String).new File.open File::NULL, "w"
+      session = PrivPage2::Session(String).new File.open File::NULL, "w"
       time = Time.utc
       session.add "1", "session", time: time
       session.start_gc interval: 0.days, max_period: 1.days
--- spec/user_repository_spec.cr
+++ spec/user_repository_spec.cr
@@ -5,26 +5,26 @@ SPEC_RESPONSE = HTTP::Server::Response.new IO::Memory.new
 
 def assert_user_repository(subdomain, user, repository, branch = nil, file = __FILE__, line = __LINE__)
   it "parses #{subdomain}", file, line do
-    user_repo = PrivPage::UserRepository.from_subdomain subdomain, SPEC_RESPONSE
-    user_repo.should be_a PrivPage::UserRepository
-    if user_repo.is_a? PrivPage::UserRepository
+    user_repo = PrivPage2::UserRepository.from_subdomain subdomain, SPEC_RESPONSE
+    user_repo.should be_a PrivPage2::UserRepository
+    if user_repo.is_a? PrivPage2::UserRepository
       user_repo.user.should eq user
       user_repo.repository.should eq repository
       user_repo.base_branch.should eq branch
     else
-      fail "expected #{PrivPage::UserRepository}"
+      fail "expected #{PrivPage2::UserRepository}"
     end
   end
 end
 
 def assert_fail_parse_user_repository(subdomain, file = __FILE__, line = __LINE__)
   it "fails to parse #{subdomain}", file, line do
-    result = PrivPage::UserRepository.from_subdomain subdomain, SPEC_RESPONSE
+    result = PrivPage2::UserRepository.from_subdomain subdomain, SPEC_RESPONSE
     result.should be_nil
   end
 end
 
-describe PrivPage::UserRepository do
+describe PrivPage2::UserRepository do
   describe "from_subdomain" do
     assert_user_repository "user--repo", "user", "repo"
     assert_user_repository "user---repo", "user", "-repo"
@@ -43,9 +43,9 @@ describe PrivPage::UserRepository do
 
   describe ".split_first_subdomain_part" do
     it "splits successfully" do
-      PrivPage::UserRepository.split_first_subdomain_part(
+      PrivPage2::UserRepository.split_first_subdomain_part(
         "sub.git.example.com").should eq({"sub", ".git.example.com"})
-      PrivPage::UserRepository.split_first_subdomain_part(
+      PrivPage2::UserRepository.split_first_subdomain_part(
         "one.two.sub.git.example.com").should eq({"one.two.sub", ".git.example.com"})
     end
   end
